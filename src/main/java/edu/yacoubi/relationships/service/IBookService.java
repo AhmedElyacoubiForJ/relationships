b4accc8aa470572ea8a5d0a444e76f837e81fe51
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-09-07 18:25:08 +0200

    book service contract

--- src/main/java/edu/yacoubi/relationships/service/IBookService.java
+++ src/main/java/edu/yacoubi/relationships/service/IBookService.java
@@ -1,4 +1,20 @@
 package edu.yacoubi.relationships.service;
 
+import edu.yacoubi.relationships.model.dto.request.BookRequestDto;
+import edu.yacoubi.relationships.model.dto.response.BookResponseDto;
+import edu.yacoubi.relationships.model.entity.Book;
+
+import java.util.List;
+
 public interface IBookService {
+    BookResponseDto addBook(BookRequestDto bookData);
+    BookResponseDto getBookDetails(Long bookId);
+    Book getBook(Long bookId);
+    List<BookResponseDto> getAllBooks();
+    void deleteBook(Long bookId);
+    BookResponseDto updateBook(Long bookId, BookRequestDto bookData);
+    BookResponseDto associateAuthorWithBook(Long bookId, Long authorId);
+    BookResponseDto disassociateAuthorFromBook(Long bookId, Long authorId);
+    BookResponseDto associateCategoryWithBook(Long bookId, Long categoryId);
+    BookResponseDto disassociateCategoryFromBook(Long bookId, Long categoryId);
 }
--- src/main/java/edu/yacoubi/relationships/service/impl/BookServiceImpl.java
+++ src/main/java/edu/yacoubi/relationships/service/impl/BookServiceImpl.java
@@ -1,8 +1,62 @@
 package edu.yacoubi.relationships.service.impl;
 
+import edu.yacoubi.relationships.model.dto.request.BookRequestDto;
+import edu.yacoubi.relationships.model.dto.response.BookResponseDto;
+import edu.yacoubi.relationships.model.entity.Book;
 import edu.yacoubi.relationships.service.IBookService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookServiceImpl implements IBookService {
+    @Override
+    public BookResponseDto addBook(BookRequestDto bookData) {
+        return null;
+    }
+
+    @Override
+    public BookResponseDto getBookDetails(Long bookId) {
+        return null;
+    }
+
+    @Override
+    public Book getBook(Long bookId) {
+        return null;
+    }
+
+    @Override
+    public List<BookResponseDto> getAllBooks() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteBook(Long bookId) {
+
+    }
+
+    @Override
+    public BookResponseDto updateBook(Long bookId, BookRequestDto bookData) {
+        return null;
+    }
+
+    @Override
+    public BookResponseDto associateAuthorWithBook(Long bookId, Long authorId) {
+        return null;
+    }
+
+    @Override
+    public BookResponseDto disassociateAuthorFromBook(Long bookId, Long authorId) {
+        return null;
+    }
+
+    @Override
+    public BookResponseDto associateCategoryWithBook(Long bookId, Long categoryId) {
+        return null;
+    }
+
+    @Override
+    public BookResponseDto disassociateCategoryFromBook(Long bookId, Long categoryId) {
+        return null;
+    }
 }
